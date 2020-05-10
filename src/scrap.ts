f34ff226ff6576ceb79ec05738934a6265f79a16
Stephen <github@step.chew.es>
2020-05-11 02:19:13 +0800

    Use request-promise instead of puppeteer. Use puppeteer only for login so Browser has a short lifespan.

--- src/scrap.ts
+++ src/scrap.ts
@@ -1,54 +1,28 @@
-import { Browser, Overrides } from 'puppeteer-core';
+import rp from 'request-promise-native';
 import { QimenType } from './types';
-import qs from 'querystring';
 import { getTypeValue } from './util/constants';
 
 const SOLAR = 1;
 
-export default (browser: Browser, csrf: string, cookies: string) => async (type: QimenType, date: Date) => {
-  const page = await browser.newPage();
-  try {
-    await page.setRequestInterception(true);
-    page.on('request', (req) => {
-      if (req.resourceType() !== 'document') {
-        req.abort();
-        return;
-      }
+export default (csrf: string, cookies: string) => async (type: QimenType, date: Date) => {
+  const options = {
+    uri: 'http://fengshui-republic.com/qimengeju',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Cookie: cookies,
+    },
+    form: {
+      '_csrf-frontend': csrf,
+      'Qimen[calendar]': SOLAR,
+      'Qimen[type]': getTypeValue(type),
+      'Qimen[en][year]': date.getFullYear(),
+      'Qimen[en][month]': date.getMonth() + 1,
+      'Qimen[en][day]': date.getDate(),
+      'Qimen[en][hour]': date.getHours(),
+      'Qimen[en][minute]': date.getMinutes(),
+    },
+  };
 
-      const http = {
-        method: 'POST',
-        headers: {
-          ...req.headers(),
-          'Content-Type': 'application/x-www-form-urlencoded',
-          Cookie: cookies,
-        },
-        postData: qs.stringify({
-          '_csrf-frontend': csrf,
-          'Qimen[calendar]': SOLAR,
-          'Qimen[type]': getTypeValue(type),
-          'Qimen[en][year]': date.getFullYear(),
-          'Qimen[en][month]': date.getMonth() + 1,
-          'Qimen[en][day]': date.getDate(),
-          'Qimen[en][hour]': date.getHours(),
-          'Qimen[en][minute]': date.getMinutes(),
-        }),
-      } as Overrides;
-
-      // console.log(http);
-      // console.log(`### ${req.url()} : ${req.resourceType()}`);
-      req.continue(http);
-    });
-
-    const response = await page.goto('http://fengshui-republic.com/qimengeju', { waitUntil: 'networkidle2' });
-
-    if (response === null) {
-      throw Error(`Response body is null`);
-    } else if (response.status() > 300) {
-      throw Error(`Response: ${response.status()}`);
-    }
-    
-    return response;
-  } finally {
-    page.removeAllListeners('request');
-  }
+  return rp(options);
 };
